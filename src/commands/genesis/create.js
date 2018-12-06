b4771ad52fbab565669f51eeb4cab8afabd514e1
SkiffCMC <skiffcmc@gmail.com>
2018-12-06 23:53:04 +0300

    added transaction types

--- src/commands/genesis/create.js
+++ src/commands/genesis/create.js
@@ -34,6 +34,16 @@ const createAccount = () => {
 };
 
 const createGenesis = (data) => {
+		const transactionTypes = {
+			SEND: 0,
+			SIGNATURE: 1,
+			DELEGATE: 2,
+			VOTE: 3,
+			MULTI: 4,
+			DAPP: 5,
+			IN_TRANSFER: 6,
+			OUT_TRANSFER: 7,
+		};
 		const transactions = data.transactions.sort((a, b) => {
 			// Place MULTI transaction after all other transaction types
 			if (
