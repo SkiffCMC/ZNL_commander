b5d624b7493dd92dcd7081f1e0e589acb0afedf8
SkiffCMC <skiffcmc@gmail.com>
2018-12-11 23:36:24 +0300

    Debug output

--- src/commands/genesis/create.js
+++ src/commands/genesis/create.js
@@ -322,7 +322,7 @@ export default class CreateCommand extends BaseCommand {
 					.digest());
 		transactions.push(transaction.castVotes({passphrase:whitelistAccount.passphrase, votes:votes},1,genesisAccount.passphrase));
 		console.log(createGenesis({transactions: transactions, keypair: keypair}));
-		console.log(transactions[number+1]);
+		console.log(block.transactions[number+1]);
 		//this.print(transaction.registerDelegate({username: 'genesis_0',}));
 	}
 }
