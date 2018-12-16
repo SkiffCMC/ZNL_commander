304768ee44889eed701204a47ba470d9a7efe57b
SkiffCMC <skiffcmc@gmail.com>
2018-12-16 22:23:44 +0300

    changed output to JSON.stringify

--- src/commands/genesis/create.js
+++ src/commands/genesis/create.js
@@ -321,7 +321,7 @@ export default class CreateCommand extends BaseCommand {
 		delegates.forEach(function tr(value,index) {
 			transactions.push(transaction.registerDelegate({username: 'genesis_'+(index+1), passphrase: value.passphrase, },1,genesisAccount.passphrase));
 			votes[index] = value.publicKey;
-			console.log('encryptedPassphrase= '+cryptography.encryptPassphraseWithPassword(value.passphrase, genesisAccount.passphrase, 1,).toString()+' for publicKey= '+value.publicKey)
+			console.log('encryptedPassphrase= '+JSON.stringify(cryptography.encryptPassphraseWithPassword(value.passphrase, genesisAccount.passphrase, 1,),null,2)+' for publicKey= '+value.publicKey)
 		});
 		let keypair = makeKeypair(crypto
 					.createHash('sha256')
